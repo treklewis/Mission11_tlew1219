533483077c65fbfdc24ecd2c69cc49b422f4d3ed
treklewis <trek.lewis@gmail.com>
2023-03-01 20:14:57 -0700

    Add project files.

---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        //Calculate number of total pages, ensure we get enough pages even when the last page won't be full, casting to double and back to int
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
